a06e7f209bef0594764be388e5fc574b392a4424
jperaltar <getajoel@gmail.com>
2015-02-10 14:25:33 +0100

    My version of the calculator

--- calculadora.py
+++ calculadora.py
@@ -7,29 +7,49 @@ Calculator with +, -, *, / operators
 
 import sys
 
-if (len(sys.argv) != 4):
-    sys.exit("\nUsage error: calculadora.py Function Operand1 Operand2\n")
+operations = {'mult': '*', 'add': '+', 'sub': '-', 'div': '/'}
 
-function = sys.argv[1]
-try:
-    operand1 = float(sys.argv[2])
-    operand2 = float(sys.argv[3])
-except ValueError:
-    sys.exit("\nError: One of the values entered is not a number\n")
 
-if (function == "mult"):
-    result = operand1 * operand2
-    operator = "*"
-elif (function == "add"):
-    result = operand1 + operand2
-    operator = "+"
-elif (function == "sub"):
-    result = operand1 - operand2
-    operator = "-"
-elif (function == "div"):
-    result = operand1 / operand2
-    operator = "/"
-else:
-    sys.exit("\nError: Function entered is not available\n")
+def mult(operand1, operand2):
+    return operand1 * operand2
 
-print str(operand1), operator, str(operand2), "=", str(result)
+
+def add(operand1, operand2):
+    return operand1 * operand2
+
+
+def sub(operand1, operand2):
+    return operand1 - operand2
+
+
+def div(operand1, operand2):
+    try:
+        return operand1 / operand2
+    except ZeroDivisionError:
+        sys.exit("\nError: Cannot solve this operation, divided by Zero\n")
+
+if __name__ == '__main__':
+
+    if (len(sys.argv) != 4):
+        sys.exit("\nUsage error: calculadora.py Operation Operand1 Operand2\n")
+
+    operation = sys.argv[1]
+    try:
+        operand1 = float(sys.argv[2])
+        operand2 = float(sys.argv[3])
+    except ValueError:
+        sys.exit("\nError: One of the values entered is not a number\n")
+
+    if operation not in operations:
+        sys.exit("\nError: Operation entered is not available\n")
+
+    if (operation == "mult"):
+        result = mult(operand1, operand2)
+    elif (operation == "add"):
+        result = add(operand1, operand2)
+    elif (operation == "sub"):
+        resutl = sub(operand1, operand2)
+    elif (operation == "div"):
+        result = div(operand1, operand2)
+
+    print str(operand1), operations[operation], str(operand2), "=", str(result)
